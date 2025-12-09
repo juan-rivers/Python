5e523ab79dcca90a579760d854af70c1777c5aab
juan-rivers <rivers.d.juan@gmail.com>
2025-12-08 16:50:00 -0800

    Add files via upload

---
+++ buffet.py
@@ -0,0 +1,12 @@
+foods = ('bacon', 'eggs', 'sausage', 'steak', 'wings')
+print("The menu here is: ")
+for food in foods:
+    print(food)
+    
+foods = (('bacon', 'chopped cheese', 'sausage', 'steak', 'pasta'))
+print("The modified menu here is: ")
+for food in foods:
+    print(food)
+
+
+
